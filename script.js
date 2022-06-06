13b21b4209ab01b532be322f6321985985e086a7
Glitch (computer-science-game-jam) <none>
2022-06-06 04:40:01 +0000

    final update! Everything is done, including the credits!

--- script.js
+++ script.js
@@ -345,16 +345,18 @@ newBackground = new Background(
   840,
   [new PortalPoint(775, 200, 1, 700, 500)],
   [new PickupPoint(350, 380, "red key", "https://cdn.glitch.global/cf3d5119-1db8-4359-89ae-64ec2566a331/redKey.png?v=1654404920320")],
+  [],
   0,
   "https://cdn.glitch.global/cf3d5119-1db8-4359-89ae-64ec2566a331/bedroom.png?v=1653515776746"
 );
 
-function Background(width, height, portalsList, pickupList, bgIndex, src) {
+function Background(width, height, portalsList, pickupList, animationList, bgIndex, src) {
   //console.log("new BG created w index " + bgIndex)
   this.width = width;
   this.height = height;
   this.portalsList = portalsList;
   this.pickupList = pickupList;
+  this.animationList = animationList
   this.bgIndex = bgIndex;
   this.src = src;
   this.checkPoints = function () {
@@ -402,11 +404,15 @@ function Background(width, height, portalsList, pickupList, bgIndex, src) {
         if (!playerInventory.some(element=> element.itemName==this.pickupList[i].itemName)){
         console.log("woah there, thats an overlap! I got me a new ITEM");
           playerInventory.push(new InventoryItem(this.pickupList[i].itemName, 0.15, this.pickupList[i].src))
+          jumpWait = Math.max(jumpWait-(20), 0) + 5
         }
       }
     }
     //console.log("no overlap")
     return 0;
+  }
+  this.renderAnimations = function(){
+    this.animationList.forEach(e=>e.render())
   }
   this.render = function () {
     let img = new Image();
@@ -458,11 +464,84 @@ function InventoryItem(itemName, scale, src){
 }
 let playerInventory = []
 
+function AnimPoint(animX, animY, frameW, frameH, animW, animH, animFrameRate, animName, animTrigger, additionalInstructions, src){
+  this.animX = animX
+  this.animY = animY
+  this.frameW = frameW
+  this.frameH = frameH
+  this.animW = animW
+  this.animH = animH
+  this.animFrameRate = animFrameRate
+  this.animName = animName
+  this.animTrigger = animTrigger
+  this.additionalInstructions = additionalInstructions
+  this.src = src
+  this.animRunning = 0
+  this.render = function(){
+    /*console.log("triggered: " + eval(this.animTrigger))
+    console.log(playerOccupiesPoint(
+          player.x,
+          player.y,
+          player.width,
+          player.height,
+          510,
+          420))
+    console.log(playerInventory.length>2)*/
+      if (eval(this.animTrigger)){
+        console.log("triggered")
+        if (this.animRunning==-1){
+          console.log("hasn't run")
+        }
+        //this.animRunning = true
+        let img = new Image();
+        img.src = this.src;
+        console.log((this.animRunning+1)*this.frameW)
+        console.log(img.width)
+        if ((this.animRunning+2)*this.frameW<=img.width){
+          this.animRunning+=1
+        }else if ((this.animRunning+1)*this.frameW<=img.width){
+          eval(this.additionalInstructions)
+console.log(this.additonalInstructions)
+        }
+        if (img.complete) {
+              ctx.drawImage(
+                img,
+                this.animRunning*this.frameW,
+                0,
+                this.frameW,
+                this.frameH,
+                this.animX - player.x + (canvas.width / 2 - player.width / 2),
+                this.animY - player.y + (canvas.height / 2 - player.height / 2),
+                this.animW,
+                this.animH);
+        }
+    }else{
+      let img = new Image();
+        img.src = this.src;
+        if (img.complete) {
+              ctx.drawImage(
+                img,
+                this.animRunning*this.frameW,
+                0,
+                this.frameW,
+                this.frameH,
+                this.animX - player.x + (canvas.width / 2 - player.width / 2),
+                this.animY - player.y + (canvas.height / 2 - player.height / 2),
+                this.animW,
+                this.animH);
+        }
+    }
+  }
+}
+
 let jumped = false;
-let jumpWait = Math.floor((10 * Math.random() + 20) * 1000);
+//let jumpWait = Math.floor((10 * Math.random() + 20) * 1000);
+let jumpWait = 80
 //jumpWait = 1
 async function jumpscare() {
-  await wait(jumpWait);
+  for (var i=0;i<jumpWait;i++){
+  await wait(1000);
+  }
   jumped = true;
   console.log("jumped");
   drawJumpScare(
@@ -490,6 +569,7 @@ function drawJumpScare(frameWidth, frameHeight, frameRate, src) {
     renderW = (renderH * frameWidth) / frameHeight;
   }
   console.log("valCalc");
+  if (jumpWait!=="a"){
   img.onload = function () {
     let frames = Math.round(img.width / frameWidth) + 1;
     console.log("drawing jump");
@@ -508,6 +588,7 @@ function drawJumpScare(frameWidth, frameHeight, frameRate, src) {
     );
   };
   img.src = src;
+  }
 }
 async function renderJumpScareFrames(
   frames,
@@ -593,6 +674,7 @@ function renderChars() {
   840,
   [new PortalPoint(775, 200, 1, 700, 500)],
   [new PickupPoint(310, 380, "red key", "https://cdn.glitch.global/cf3d5119-1db8-4359-89ae-64ec2566a331/redKey.png?v=1654404920320")],
+        [],
   0,
   "https://cdn.glitch.global/cf3d5119-1db8-4359-89ae-64ec2566a331/bedroom.png?v=1653515776746"
 );
@@ -619,7 +701,9 @@ function renderChars() {
       borders.push(new Wall(3.8, 4.5, 4, 4.3, "#"));
       borders.push(new Wall(4, 4.3, 4.3, 4.3, "#"));
       borders.push(new Wall(4.3, 4.3, 4.3, 4, "#"));
+      
       borders.push(new Wall(4.3, 4, 5.9, 4, "#"));
+      
       borders.push(new Wall(5.9, 4, 5.9, 4.3, "#"));
       borders.push(new Wall(5.9, 4.3, 6.2, 4.3, "#"));
       borders.push(new Wall(6.2, 4.3, 6.5, 4.5, "#"));
@@ -642,8 +726,13 @@ function renderChars() {
       newBackground = new Background(
         1000,
         830,
-        [new PortalPoint(775, 525, 0, 775, 300), new PortalPoint(120, 615, 2, 740, 750)],
+        [new PortalPoint(775, 525, 0, 775, 300), new PortalPoint(120, 615, 2, 740, 750),  new PortalPoint(510, 360, 3, 180, 180)],
         [new PickupPoint(960, 700, "yellow key", "https://cdn.glitch.global/cf3d5119-1db8-4359-89ae-64ec2566a331/yellowKey.png?v=1654404937365")],
+         [new AnimPoint(434, 318, 50, 36, 154, 115, 1, "endingDoor", 
+          "(playerOccupiesPoint(player.x,player.y,player.width,player.height,510,420)&&playerInventory.length>2)", 
+          //"(playerOccupiesPoint(player.x,player.y,player.width,player.height,510,420))", 
+          "borders.splice(19, 1, (new Wall(4.3, 4, 4.3, 3.18, '#')));borders.splice(20, 0, (new Wall(4.3, 3.18, 5.9, 3.18, '#')));borders.splice(21, 0, (new Wall(5.9, 3.18, 5.9, 4, '#')))",
+          "https://cdn.glitch.global/cf3d5119-1db8-4359-89ae-64ec2566a331/hallwayDoorArray.png?v=1654485020612")],
         1,
         "https://cdn.glitch.global/cf3d5119-1db8-4359-89ae-64ec2566a331/hallway?v=1654090154156"
       );
@@ -675,20 +764,39 @@ function renderChars() {
         830,
         [new PortalPoint(950, 800, 1, 160, 630)],
         [new PickupPoint(100, 625, "green key", "https://cdn.glitch.global/cf3d5119-1db8-4359-89ae-64ec2566a331/greenKey.png?v=1654404928746")],
+       [],
         1,
         "https://cdn.glitch.global/cf3d5119-1db8-4359-89ae-64ec2566a331/playroom?v=1654265997147"
       );
+    }else if(newBackgroundToSet == 3) {
+      newBackground = new Background(
+        360,
+        360,
+        [],
+        [],
+       [],
+        2,
+        "https://cdn.glitch.global/cf3d5119-1db8-4359-89ae-64ec2566a331/credits2.png?v=1654489308956"
+      );
+      console.log("Everything neutralized")
+      player.height = 0
+      player.maxSpeed = 0
+      handheldItemsList.forEach(e=>e.renderedHeight=0)
+      playerInventory.forEach(e=>e.scale = 0)
+      jumpWait = "a"
     }
   }
   //console.log("bg pre render: " + newBackground.src)
   newBackground.render();
+  newBackground.renderAnimations()
   //console.log("bg post render: " + newBackground.bgIndex);
   player.getKeys();
   player.render();
   borders.forEach((e) => e.render());
   handheldItemsList.forEach((e) => e.render());
+  if (jumpWait!=="a"){
   flashlight.render();
-  
+  }
   if (!jumped){
   playerInventory.forEach((inventoryItem, index)=> inventoryItem.render(index))
   }
